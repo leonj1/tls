bc5cbc55edcac43c3d5990fd04aaf17ed14c88a5
Jose Leon <leonj1@gmail.com>
2017-08-18 07:48:30 -0400

    Adding log rolling

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java ${JAVA_OPTS} -jar /app/tls-1.0-SNAPSHOT.jar --enable.ssl=true --http.server.port=443
+java ${JAVA_OPTS} -Dlog4j.configuration=log4j2.xml -jar /app/tls-1.0-SNAPSHOT.jar --enable.ssl=true --http.server.port=443
 
